dfcae77f9609a95088187fe8f5f8b1556031776f
Taiki Sonoyama <109128636+fullpowerz1@users.noreply.github.com>
2023-02-22 08:09:55 +0000

    san

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  # ログイン後のpath
   def after_sign_in_path_for(resource)
     about_path
   end
 
+  # ログアウト後のpath
   def after_sign_out_path_for(resource)
     about_path
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # アソシエーション 1:N
+  has_many :post_images, dependent: :destroy
+
 end
