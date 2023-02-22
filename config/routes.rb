47df833a2eb6b822deda82b2451273337227a12c
Taiki Sonoyama <109128636+fullpowerz1@users.noreply.github.com>
2023-02-22 05:26:46 +0000

    igunister

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
+  get 'homes/about' => 'homes#about',as: 'about'
   # 徐々に加えていく。
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
