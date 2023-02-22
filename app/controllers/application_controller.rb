6256d8cbc4a6ee14bc632786dbdbf55d5fbe0ab4
Taiki Sonoyama <109128636+fullpowerz1@users.noreply.github.com>
2023-02-22 12:15:14 +0000

    jin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 
   # ログイン後のpath
   def after_sign_in_path_for(resource)
-    about_path
+    post_images_path
   end
 
   # ログアウト後のpath
--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -2,4 +2,14 @@ class PostImage < ApplicationRecord
 
   has_one_attached :image
   belongs_to :user
+
+  # 特定の処理を名前で呼び出すメソッド
+  def get_image
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+      image
+  end
+
 end
