abbc6c6bb55aa0a5be568e9f1280e8de4a1c3976
Taiki Sonoyama <109128636+fullpowerz1@users.noreply.github.com>
2023-02-23 03:25:17 +0000

    sakujyo

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -19,6 +19,13 @@ class PostImagesController < ApplicationController
     redirect_to post_images_path
   end
 
+  # 削除機能
+  def destroy
+    post_image = PostImage.find(params[:id])
+    post_image.destroy
+    redirect_to post_images_path
+  end
+
   # 投稿データのストロングパラメータ
   private
 
